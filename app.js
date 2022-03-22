ec52b52128a78801d71ddbf3b3db4f81434c8bd0
Fiaz56 <mdfayyu786@gmail.com>
2022-03-23 00:14:40 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const form = document.querySelector("#searchForm");
+
+form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+    const userInput = form.elements.query.value;
+    const config = { params: { q: userInput } };
+    const res = await axios.get("https://api.tvmaze.com/search/shows", config);
+    makeImages(res.data);
+    form.elements.query.value = "";
+});
+
+const makeImages = (shows) => {
+    for (let result of shows){
+        if(result.show.image){
+            const img = document.createElement("img");
+            img.src = result.show.image.medium;
+            document.body.appendChild(img);
+        }
+    }
+};
